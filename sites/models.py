373c9cfd19fd2d22ff5f8efd60b83ee48349df4b
Tiran132_2 <egor@bk.ru>
2019-05-19 18:49:25 +0700

    DB

--- sites/models.py
+++ sites/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Site(models.Model):
     title = models.CharField(max_length=200)
-    inf = models.TextField()
+    info = models.TextField()
     img = models.CharField(max_length=1000)
     category = models.ForeignKey(
         to=Category,
@@ -15,3 +15,12 @@ class Site(models.Model):
 
     def __str__(self):
         return self.title[:20]
+
+
+class Likes(models.Model):
+    num = models.IntegerField()
+    likes = models.ForeignKey(
+        to=Site,
+        related_name="likes",
+        on_delete=models.CASCADE()
+    )
